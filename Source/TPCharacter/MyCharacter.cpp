dee3ee35333bb25def2a1c60a5ec68f52a726311
Mridul Roy <mridulroy23@gmail.com>
2023-08-02 00:35:19 +0600

    Modified AMycharacter.cpp - Adjusted death condition

--- Source/TPCharacter/MyCharacter.cpp
+++ Source/TPCharacter/MyCharacter.cpp
@@ -17,6 +17,8 @@ AMyCharacter::AMyCharacter()
 	//The character will rotate to the direction of its movement
 	GetCharacterMovement()->bOrientRotationToMovement = true;
 	GetCharacterMovement()->RotationRate = FRotator(0.f, 540.f, 0.f);
+	GetCharacterMovement()->AirControl = 100.f;
+	GetCharacterMovement()->JumpZVelocity = 800.f;
 
 	//Setting up Spring arm component
 	cameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("Camera Boom"));
@@ -27,14 +29,31 @@ AMyCharacter::AMyCharacter()
 	//Main third person camera
 	followCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Follow Camera"));
 	followCamera->bUsePawnControlRotation = false;
+	followCamera->FieldOfView = 100.f;
 	followCamera->SetupAttachment(cameraBoom, USpringArmComponent::SocketName);
+
+	//Default Health of character
+	health = 100.f;
+	//healthDecreaseRate = 20.f;
+	bIsDead = false;
+
+	if (GetCapsuleComponent())
+	{
+		GetCapsuleComponent()->OnComponentBeginOverlap.AddDynamic(this, &AMyCharacter::BeginOverlap);
+	}
+	
 }
 
 // Called when the game starts or when spawned
 void AMyCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	if (playerHUD)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player HUD Found"));
+		playerWidget = CreateWidget(GetWorld(), playerHUD);
+		playerWidget->AddToViewport(1);
+	}
 }
 
 
@@ -43,6 +62,31 @@ void AMyCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (GetCharacterMovement()->IsFalling())
+	{
+		GetCharacterMovement()->RotationRate = FRotator(0.f, 80.f, 0.f);
+	}
+	else
+	{
+		GetCharacterMovement()->RotationRate = FRotator(0.f, 540.f, 0.f);
+	}
+
+	health -= DeltaTime * healthDecreaseRate;
+	
+	if (health <= 0 && !bIsDead)
+	{
+		bIsDead = true;
+		UE_LOG(LogTemp, Warning, TEXT("Collided with Collectible"));
+		GetMesh()->SetSimulatePhysics(true);
+
+		FTimerHandle UnusedHandle;
+		GetWorldTimerManager().SetTimer(UnusedHandle, this,
+			&AMyCharacter::RestartGame, 3.0f, false);
+
+		GetController()->SetIgnoreMoveInput(true);
+		
+		
+	}
 }
 
 // Called to bind functionality to input
@@ -80,4 +124,20 @@ void AMyCharacter::MoveRight(float axisValue)
 	const FRotator YawRotation(0, Rotation.Yaw, 0);
 	const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
 	AddMovementInput(Direction, axisValue);
+}
+
+void AMyCharacter::BeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Collided with Collectible"));
+	if (OtherActor->ActorHasTag("Collectible"))
+	{
+		health += 10.f;
+		OtherActor->Destroy();
+	}
+}
+
+void AMyCharacter::RestartGame()
+{
+	UGameplayStatics::OpenLevel(this, FName(*GetWorld()->GetName()), false);
+
 }
--- Source/TPCharacter/MyCharacter.h
+++ Source/TPCharacter/MyCharacter.h
@@ -4,6 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include <Components/CapsuleComponent.h>
+#include <Components/SkeletalMeshComponent.h>
+#include <GameFramework/Actor.h>
+#include <Kismet/GameplayStatics.h>
+#include <Blueprint/UserWidget.h>
 #include <Camera/CameraComponent.h>
 #include <GameFramework/SpringArmComponent.h>
 #include <GameFramework/CharacterMovementComponent.h>
@@ -25,11 +30,29 @@ public:
 	UPROPERTY(VisibleAnywhere, BlueprintREadOnly, Category = "Camera Config")
 		USpringArmComponent* cameraBoom;
 
+	UPROPERTY(EditAnywhere, Category = "UI Config")
+		TSubclassOf<UUserWidget>playerHUD;
+	
+	class UUserWidget* playerWidget;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	float health;
+
+	UPROPERTY(EditAnywhere, Category = "UI Config")
+	float healthDecreaseRate;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	bool bIsDead;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 	virtual void MoveForward(float axisValue);
 	virtual void MoveRight(float axisValue);
+	virtual void RestartGame();
+
+	UFUNCTION()
+	virtual void BeginOverlap(class UPrimitiveComponent* OverlappedComponent, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 public:	
 	// Called every frame
